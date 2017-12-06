28f3c8a2dcf3ac826f3127e86505045ce6861f5c
acm7552 <acm7552@g.rit.edu>
2017-12-06 18:08:58 -0500

    Interactive1 Update

--- js/project4.js
+++ js/project4.js
@@ -10,14 +10,22 @@ var sandrect = s.rect(0, 300, 5000, 1040);
 
 
 
-var sail = s.rect("50%", "40%", "8vw", "8vw");
+var mast = s.rect(0, 0, 10, 120);
+var sail = s.rect(0, 0, 100, 100);
 
-var mast = s.rect("49%", "40%", "1vw", "17vw");
-
-var hull = s.circle("50%", "55%", "10vw");
-var hullMask = s.rect("15%", "60%", "65vw", "7vw");
+var hull = s.circle(10, 100, 120);
+var hullMask = s.rect(-140, 110, 6000, 70);
 
 
+var g = s.group(sail, mast, hull, hullMask);
+
+var board = document.getElementById("board");
+
+var centerX = board.offsetWidth;
+var centerY = board.offsetHeight;
+
+console.log(centerY);
+console.log(5);
 
 
 
@@ -54,27 +62,36 @@ sail.attr({
 
 
 
+
+
 //FUNCTIONS
 
 //We loop a bobbing animation forever for the first interactible
 var aLoop = function(y){
   
-  
+    centerX = board.offsetWidth;
+    centerY = board.offsetHeight;
     var newY = y === 350 ? 300: 350
+
+    g.animate({
+        transform: 'translate(' + 2 * centerX/ 5 + ',' + (200 - ((newY - y)/4)) + ")"}, 2400, );
+    
+    hullMask.animate
+    ( {height: 50 - y/100}, 2400 );
 
     sandrect.animate
     ({y: y}, 2400, function(){
         aLoop(newY)
 
     })
-  
-   
+     
 }
 
 var Bob = function(){
+    
     aLoop(350);
 }
-
+g.animate({ transform: 'translate(' + 2 * centerX/ 5 + ',' + centerY/3 + ')'}, 0, );
 Bob()
 })
 
